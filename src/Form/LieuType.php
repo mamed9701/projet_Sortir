e9d300fe5b49171793a7a65e449426efce9e7491
mamed9701 <mhammed9701@live.it>
2021-03-11 13:00:42 +0100

    Modif

--- src/Form/LieuType.php
+++ src/Form/LieuType.php
@@ -12,7 +12,9 @@ class LieuType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
+            ->add('nom', null, [
+                'label' => 'Ville',
+            ])
             ->add('rue')
             ->add('latitude')
             ->add('longitude')
--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -2,36 +2,62 @@
 
 namespace App\Form;
 
-use App\Entity\Sortie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use function Symfony\Bridge\Twig\Extension\twig_is_selected_choice;
 
 class SortieType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('url_photo')
             ->add('nom', null, [
-                'label' => 'Nom de la sortie'
+                'label' => 'Nom de la sortie : '
             ])
             ->add('date_debut', null, [
-                'label' => 'Date et heure de la sortie'
+                'label' => 'Date et heure de la sortie : '
+            ])
+            ->add('date_cloture', DateType::class, [
+                'format' => 'dd-MM-yyyy',
+                'label' => "Date limite d'inscription : "
+            ])
+            ->add('nb_inscriptions_max', null, [
+                'label' => 'Nombre de places : '
             ])
 
-            ->add('duree')
-            ->add('date_cloture', DateType::class, [
-                'format' => 'dd-MM-yyyy'
+            ->add('duree', null, [
+                'label' => 'Durée : '
             ])
-            ->add('nb_inscriptions_max')
-            ->add('description_infos')
-            ->add('url_photo')
-            ->add('organisateur')
-            ->add('lieux_no_lieu')
-            ->add('site_organisateur', null, [
-                'choice_label' => 'nom'
+            ->add('description_infos', TextareaType::class, [
+                'label' => 'Descritpion et infos : '
             ])
+
+            ->add('site_organisateur', EntityType::class, [
+                'label' => 'Ville organisatrice : ',
+                'class' => Site::class,
+
+            ])
+//            ->add('organisateur')
+
+            ->add('lieux_no_lieu', LieuType::class)
+
+//            ->add('lieux_no_lieu', null, [
+//                'label' => 'Rue',
+//                'choice_label' => function(Lieu $lieu) {
+//                    return sprintf('%s', $lieu->getRue());
+//                }
+//            ])
+
+            ->add('lieux_no_lieu', null, [
+                'label' => 'Lieu',
+                'choice_label' => 'Lieu'
+            ])
+
             ->add('etats_no_etat', null, [
                 'choice_label' => 'libelle'
             ])
